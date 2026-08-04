ea74dc15369a98f900d09698239ebf6ccea3d8ef
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-08-04 13:12:35 +0530

    Score

--- track/M02-Introduction to Java/T01-Intro to java/Numbers.java
+++ track/M02-Introduction to Java/T01-Intro to java/Numbers.java
@@ -2,13 +2,25 @@
 public class Numbers {
 
     public static void main(String[] args) {
-        int num = 0;
+        int num = -7;
+        int firstScore = 18;
+        int secondScore = 25;
         if (num > 0) {
             System.out.println("Positive");
         } else if (num < 0) {
             System.out.println("Negative");
         } else {
             System.out.println("Zero");
+        }
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+        if (firstScore >= secondScore) {
+            System.out.println("Larger score: " + firstScore);
+        } else {
+            System.out.println("Larger score: " + secondScore);
         }
     }
 }
