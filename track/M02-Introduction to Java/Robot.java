9720874e7371bb837d002f3fbbffa3aad2811447
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-08-13 11:02:46 +0530

    Robot

---
+++ track/M02-Introduction to Java/Robot.java
@@ -0,0 +1,10 @@
+public class Robot {
+    void speak(){
+        System.out.println("Beep Beep! Java is a Superpower!");
+    }
+
+    public static void main(String[]args){
+        Robot r = new Robot();
+        r.speak();
+    }
+}
